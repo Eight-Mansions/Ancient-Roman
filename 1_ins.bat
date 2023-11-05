de3792ab5e531555779c6f9640ebea5546b661a6
esperknight <esperknight@yahoo.com>
2023-11-04 21:26:59 -0400

    Added script formatting to insertion.  Formatting kinda works!

--- 1_ins.bat
+++ 1_ins.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Formatting disc 1 scripts...
-tools\ancient_roman_script_insert.exe orig\D1_S00 ins\D1_S00 sjis.tbl
+tools\ancient_roman_script_insert.exe orig\D1_S00 ins\D1_S00 sjis.tbl code\ancient-roman\code\font.cpp 255
 
 tools\ancient_roman_dat_insert.exe ins
 
