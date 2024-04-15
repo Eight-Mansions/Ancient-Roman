3ed689610286514ec25da1270cd25bf25e0fb9f5
esperknight <esperknight@yahoo.com>
2024-04-15 00:36:50 -0400

    Forgot to check the create patch batch file :D

---
+++ 0_create_patch.bat
@@ -0,0 +1,2 @@
+del ancient-roman-psx-patcher\patch_data\patch.xdelta
+tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\Ancient-Roman-Disc-1_original.bin cd\Ancient-Roman-Disc-1_working.bin ancient-roman-psx-patcher\patch_data\patch.xdelta
