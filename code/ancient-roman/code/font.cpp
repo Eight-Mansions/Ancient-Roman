d6836f00ad972f21481ace97bff446384fafb16c
esperknight <esperknight@yahoo.com>
2024-06-12 17:12:52 -0400

    Added ' and " to small punctuation

--- code/ancient-roman/code/font.cpp
+++ code/ancient-roman/code/font.cpp
@@ -111,12 +111,12 @@ const u8 dialogueLetterWidths[] = {
 const u8 menuLetterWidths[] = {
 	0x03, //  
 	0x02, // !
-	0x03, // "
+	0x04, // "
 	0x07, // #
 	0x05, // $
 	0x06, // %
 	0x05, // &
-	0x01, // '
+	0x02, // '
 	0x03, // (
 	0x03, // )
 	0x07, // *
