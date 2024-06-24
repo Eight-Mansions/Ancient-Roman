1e56f8527292f7e4be5807b34c5fb38bd1642866
esperknight <esperknight@yahoo.com>
2024-06-23 20:21:44 -0400

    Updated Ancient Roman to work with both bad and good redumps

--- 0_create_patch.bat
+++ 0_create_patch.bat
@@ -1,7 +1,9 @@
-del ancient-roman-psx-patcher\patch_data\patch-1.xdelta
-del ancient-roman-psx-patcher\patch_data\patch-2.xdelta
-tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\Ancient-Roman-Disc-1_original.bin cd\Ancient-Roman-Disc-1_working.bin ancient-roman-psx-patcher\patch_data\patch-d1.xdelta
-tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\Ancient-Roman-Disc-1_original-other.bin cd\Ancient-Roman-Disc-1_working.bin ancient-roman-psx-patcher\patch_data\patch-d1-other.xdelta
+del release\patch_data\patch-d1.xdelta
+del release\patch_data\patch-d2.xdelta
+del release\patch_data\patch-d1-other.xdelta
+del release\patch_data\patch-d2-other.xdelta
+tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\Ancient-Roman-Disc-1_original.bin cd\Ancient-Roman-Disc-1_working.bin release\patch_data\patch-d1.xdelta
+tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\Ancient-Roman-Disc-1_original-other.bin cd\Ancient-Roman-Disc-1_working.bin release\patch_data\patch-d1-other.xdelta
 
-tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\Ancient-Roman-Disc-2_original.bin cd\Ancient-Roman-Disc-2_working.bin ancient-roman-psx-patcher\patch_data\patch-d2.xdelta
-tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\Ancient-Roman-Disc-2_original-other.bin cd\Ancient-Roman-Disc-2_working.bin ancient-roman-psx-patcher\patch_data\patch-d2-other.xdelta
+tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\Ancient-Roman-Disc-2_original.bin cd\Ancient-Roman-Disc-2_working.bin release\patch_data\patch-d2.xdelta
+tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\Ancient-Roman-Disc-2_original-other.bin cd\Ancient-Roman-Disc-2_working.bin release\patch_data\patch-d2-other.xdelta
