04b02340dac24877d193184c95aa65395350b45a
esperknight <esperknight@yahoo.com>
2024-06-30 23:59:02 -0400

    Updated formatting and added missing line for disc 2

--- 1_ins.bat
+++ 1_ins.bat
@@ -1,8 +1,8 @@
 echo Formatting disc 1 scripts...
-tools\ancient_roman_script_insert.exe orig\D1_S00 ins\D1_S00 sjis.tbl code\ancient-roman\code\font.cpp 271 240
+tools\ancient_roman_script_insert.exe orig\D1_S00 ins\D1_S00 sjis.tbl code\ancient-roman\code\font.cpp 265 240
 
 echo Formatting disc 2 scripts...
-tools\ancient_roman_script_insert.exe orig\D2_S00 ins\D2_S00 sjis.tbl code\ancient-roman\code\font.cpp 271 240
+tools\ancient_roman_script_insert.exe orig\D2_S00 ins\D2_S00 sjis.tbl code\ancient-roman\code\font.cpp 265 240
 
 tools\ancient_roman_dat_insert.exe ins
 
